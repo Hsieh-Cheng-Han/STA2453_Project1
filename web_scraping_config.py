d763d3c6b23650f6f16aeabb8edd843e10a88008
Hsieh-Cheng-Han <31091957+Hsieh-Cheng-Han@users.noreply.github.com>
2020-11-28 14:58:44 +0800

    add "what you bring to" to match_list

--- web_scraping_config.py
+++ web_scraping_config.py
@@ -2,4 +2,4 @@ keyword_set = {"data+analyst", "data+scientist", "data+engineer", "software+engi
 columns = ["job_title", "job_category", "company_name", "requirements", "location","industry", "salary", "post_date"]
 jobs_per_page=15
 location = 'Toronto ON'
-match_list = ["will have","must haves","qualifications","required","requirements","what you'll bring","who you are","skills", "experience", "who are you?"]
+match_list = ["will have","must haves","qualifications","required","requirements","what you'll bring","who you are","skills", "experience", "who are you?", "what you bring to"]
