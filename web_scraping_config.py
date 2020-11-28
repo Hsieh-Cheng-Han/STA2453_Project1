f601a91f69226a876de15ac3cdcdb252539913fe
Azure-Yan <72534264+Azure-Yan@users.noreply.github.com>
2020-11-27 22:56:07 -0500

    add job_category and date in columns list

--- web_scraping_config.py
+++ web_scraping_config.py
@@ -1,5 +1,5 @@
 keyword_set = {"data+analyst", "data+scientist", "data+engineer", "software+engineer","software+developer","statistician"}
-columns = ["job_title", "company_name", "requirements", "location","industry", "salary"]
+columns = ["job_title", "job_category", "company_name", "requirements", "location","industry", "salary", "post_date"]
 jobs_per_page=15
 location = 'Toronto ON'
-match_list = ["will have","must haves","qualifications","required","requirements","what you'll bring","who you are","skills", "experience", "who are you?","what you bring to"]
+match_list = ["will have","must haves","qualifications","required","requirements","what you'll bring","who you are","skills", "experience", "who are you?"]
